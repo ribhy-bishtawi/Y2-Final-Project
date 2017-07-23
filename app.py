58d8b3e10579aff2d377ad09b5338bf2b697a99a
Majd Marie <majd.mar3e@gmail.com>
2017-07-23 10:13:09 +0300

    adsa

--- app.py
+++ app.py
@@ -2,48 +2,45 @@ from flask import Flask, render_template, request, redirect, url_for
 import dataset
 
 app = Flask(__name__)
+
 db=dataset.connect("postgres://xsablmlvdcwygk:304f665b117ed37f455b90d9ec7ea4235c2ea5a088b24fb094114343473c3ebb@ec2-54-235-119-27.compute-1.amazonaws.com:5432/d7489ilia0g0mf")
 
 @app.route('/')
 @app.route('/home')
 def homepage():
-	return render_template('home.html')
+	return render_template('/home.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def registerpage():
+	return render_template("register.html")
+
+
+@app.route('/signup',  methods=["get","post"] )
+def signup():
+	if request.method =="GET":
+		return render_template("login.html")
+	else:
+		form = request.form 
+		firstname = form["firstname"]
+		lastname = form["lastname"]
+		email = form["email"]
+		password = form["password"]
+		hometown = form["hometown"]
+		website = form["website"]
+		signupTable = db["signup"]
+		entry = { "name":name, "email": email, "message": message }
+		signupTable.insert(entry)
+		print list(signupTable.all())
+		return render_template("login.html", firstname=firstname, lastname=lastname, email=email , password=password, hometown=hometown, website=website )
 
 # TODO: route to /list
 
 # TODO: route to /feed
 
-@app.route('/login' ,methods=['POST',"GET"])
-def login():
-    form = request.form
-    password = form["password-l"]
-    email = form["email-l"]
-    loginTable = db["login-n"]
-    entry = {"password": password, "email": email}
-    loginTable.insert(entry)
-    print list(loginTable.all())
-    return render_template("register.html" , email=email ,password=password)
+# TODO: route to /register
 
-
-    if loginTable.find(email='email'):
-        print "already in use"
-
-
-@app.route("/register")
-def register():
-    return render_template("register.html")
 # TODO: route to /error
 
-if __name__ == "__main__":
-    app.run(port=3000)
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+	app.run(port=3000)
