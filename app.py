3d6f513b51c397411dbab41ece5eb38858b33860
ribhy bishtawi <ribhy2001@gmail.com>
2017-07-24 12:45:08 +0300

    ribhy

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 import dataset
-
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 db=dataset.connect("postgres://xsablmlvdcwygk:304f665b117ed37f455b90d9ec7ea4235c2ea5a088b24fb094114343473c3ebb@ec2-54-235-119-27.compute-1.amazonaws.com:5432/d7489ilia0g0mf")
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home.html')
 def homepage():
-	return render_template('/home.html')
+
+	if 'email' in session :
+		return render_template("home.html")+'Logged in as %s' %(session['email'])
+	else:
+		return render_template("home.html")
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def registerpage():
-	return render_template("register.html")
+	return render_template("reg.html")
 
 
-@app.route('/signup',  methods=["get","post"] )
+@app.route('/signup',  methods=['get','post'] )
 def signup():
 		if request.method=="get":
-			return render_template("register.html")
+			return render_template("reg.html")
 		else:
 			form = request.form
 			firstname = form["firstname"]
@@ -28,13 +32,45 @@ def signup():
 			password = form["password"]
 			hometown = form["hometown"]
 			website = form["website"]
+
 			signupTable = db["signup"]
-			entry = { "lastname":lastname, "email": email, "firstname": firstname, password:"password",hometown:"hometown",website:"website"}
+			entry = { "lastname":lastname, "email": email, "firstname": firstname, "password":password,"hometown":hometown,"website":website}
 			if len(list(signupTable.find(email=email)))==0:
 				signupTable.insert(entry)
-				return render_template("login.html", email=email, firstname=firstname,lastname=lastname, password=password, hometown=hometown,website=website)
+				return render_template("reg.html", email=email, firstname=firstname,lastname=lastname, password=password, hometown=hometown,website=website)
 			else:
 				return "user already"
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+		if request.method == "get":
+			return render_template("login.html")
+		else:
+			form = request.form
+			email = form["email-l"]
+			password=form["password-l"]
+			signupTable = db["signup"]
+			# print list(signupTable.find(email="moh@mail.com"))
+			entry = {"email": email,"password":password}
+			if len(list(signupTable.find(email=email,password=password))) > 0 :
+				session['email'] = request.form['email-l']
+				signupTable.insert(entry)
+				return redirect("/home.html")
+
+			else:
+				return"false"
+
+@app.route("/login.html", methods=['GET', 'POST'])
+def loginn():
+	return  render_template("login.html")
+@app.route('/reg.html')
+def reg():
+	return  render_template("reg.html")
+@app.route('/logout', methods=['post', 'get'])
+def logout():
+	session.pop('email', None)
+	return redirect("/login.html")
+
 
 
 
@@ -42,23 +78,7 @@ def signup():
 
 # TODO: route to /feed
 
-# @app.route('/login' ,methods=['POST',"GET"])
-# def login():
-#         form = request.form
-#         password = form["password-l"]
-#         email = form["email-l"]
-#         loginTable = db["login-n"]
-#         entry = {"password": password, "email": email}
-#         loginTable.insert(entry)
-#         print list(loginTable.all())
-#         return render_template("register.html" , email=email ,password=password)
-#
-#
-#
-#
-# @app.route("/loginn")
-# def register():
-#     return render_template("register.html")
+
 # # TODO: route to /error
 
 if __name__ == '__main__':
